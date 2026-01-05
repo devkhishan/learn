02ef66118d679c56c0dc198dec14ab32cc5b94da
devkhishan <devk21111.cd@rmkec.ac.in>
2026-01-05 23:55:30 +0530

    Cargo Project Init

---
+++ rust/hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d
@@ -0,0 +1,5 @@
+/home/dev/Github/learn/rust/hello_cargo/target/debug/deps/libhello_cargo-4f8f4b4faaf615cd.rmeta: src/main.rs
+
+/home/dev/Github/learn/rust/hello_cargo/target/debug/deps/hello_cargo-4f8f4b4faaf615cd.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/dev/Github/learn/rust/hello_cargo/target/debug/hello_cargo: /home/dev/Github/learn/rust/hello_cargo/src/main.rs
