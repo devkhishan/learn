cfb627e8813adc0d8698c43b66da490378f450f0
devkhishan <devk21111.cd@rmkec.ac.in>
2026-01-09 22:17:23 +0530

    Project Completed

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -7,22 +7,28 @@ fn main() {
 
     let secret_number = rand::rng().random_range(1..=100);
 
-    println!("The secress number is: {secret_number}");
+    loop {
+        println!("Please enter your guess: ");
 
-    println!("Please enter your guess: ");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You won"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You won");
+                break;
+            }
+        }
     }
 }
