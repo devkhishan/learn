17ac6f6cf58f145f8f7223fc93d9396a3dfe6809
devkhishan <devk21111.cd@rmkec.ac.in>
2026-01-14 23:41:23 +0530

    Numeric operations

--- rust/data_types/src/main.rs
+++ rust/data_types/src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
     let x = 34.3234; // f64
     let x: f32 = 982.23; // f32
+
+    // numeric operations
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+
+    let remainder = 43 % 5;
 }
