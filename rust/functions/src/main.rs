a166bb0de2abaa20893bce50dbb8159e0d1d24ff
devkhishan <devk21111.cd@rmkec.ac.in>
2026-02-04 23:20:49 +0530

    Function with parameter and return types

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
     another_function();
+    let addition = add(10, 12);
+    println!("Addition : {addition}");
 }
 
 fn another_function() {
     println!("Printing from another method");
+}
+
+fn add(x: usize, y: usize) -> usize {
+    x + y
 }
