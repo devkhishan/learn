d6d9c5419fc00e85342f68d9a5fb21b860ad94de
devkhishan <devk21111.cd@rmkec.ac.in>
2026-02-06 22:43:22 +0530

    loop and for

--- rust/branches/src/main.rs
+++ rust/branches/src/main.rs
@@ -1,20 +1,47 @@
+use std::io;
+
 fn main() {
-    let number = 3;
-    if number > 5 {
-        println!("Number is greater than 5");
-    } else {
-        println!("Number is lesser than or equal to 5");
-    }
+    //let counter = 0;
+    // let number = 3;
+    /*
 
-    let mut counter = 0;
-
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
+        if number > 5 {
+            println!("Number is greater than 5");
+        } else {
+            println!("Number is lesser than or equal to 5");
         }
-    };
 
-    println!("The result is {result}");
+        let result = loop {
+            counter += 1;
+
+            if counter == 10 {
+                break counter * 2;
+            }
+        };
+
+        println!("The result is {result}");
+
+        let boolean: bool = true;
+        let value = if boolean { 5 } else { 2 };
+        println!("Ternary {value}");
+
+        'count: loop {
+            let mut inner_counter = 0;
+            counter += 1;
+            println!("Counter: {counter}");
+            loop {
+                inner_counter += 1;
+                println!("Inner Counter: {inner_counter}");
+                if inner_counter == 10 {
+                    break;
+                }
+                if counter == 2 {
+                    break 'count;
+                }
+            }
+        }
+    */
+    for number in (1..4).rev() {
+        println!("Current: {number}");
+    }
 }
